881d19ea76acd1ac4bfb85f1c7b3be7a0f25bbbe
JohnTorricoC <dark.nightmare1309@gmail.com>
2021-09-29 14:45:56 -0400

    Fix CS-01
    Removido codigo comentado en la linea 3

--- src/layouts/LoaderLogin.jsx
+++ src/layouts/LoaderLogin.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from "react";
-/* import { useAuth0 } from "@auth0/auth0-react";
-import Axios from "axios"; */
 import { Dimmer, Loader } from "semantic-ui-react";
 import NoAutorizado from "./VistaNoAutorizado";
 import { useHistory } from "react-router-dom";
